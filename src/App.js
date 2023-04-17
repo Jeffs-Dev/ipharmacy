252eafd67233c02c1836469b0b5395f7af597791
Jefferson Soares <soares.jeffersonthadeu@gmail.com>
2023-04-17 17:35:38 -0300

    feat: header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-   <>
-   <h1> Header </h1>
-        <Outlet/>
-   <h1> Footer  </h1>
-   </>
+    <>
+    <div className="px-4">
+
+      <Header />
+        <Outlet />
+      <h1> Footer </h1>
+    </div>
+    </>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+import logo from '../../common/logo.svg';
+
+const Header = () => {
+  return (
+    <>
+        
+        <header className='flex justify-between items-center'>
+        <Link>
+            <img src={logo} alt='Logo' />
+        </Link>
+
+        <ul className='flex gap-4'>
+            <li> <Link to='/'> Home </Link>   </li>
+            <li> <Link to='/register'> Register </Link>   </li>
+            <li> <Link to='/data' > Data </Link>   </li>
+        </ul>
+
+
+
+        </header>
+    </>
+  )
+}
+
+export default Header;
