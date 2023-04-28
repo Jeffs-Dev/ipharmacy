ac33bace4189c5d1397166a048451e084ddcfbf6
Jefferson Soares <soares.jeffersonthadeu@gmail.com>
2023-04-28 09:16:25 -0300

    chore: remove test code

--- src/App.js
+++ src/App.js
@@ -5,47 +5,6 @@ import DataRoutesProvider from "./context/DataRoutes";
 
 function App() {
 
-  let arr = [
-    {
-      title: "Banana",
-      description: "...",
-      price: 1,
-      category: 1,
-      id: 1
-    },
-    {
-      title: "Watermelon",
-      description: "...",
-      price: 2,
-      category: 1,
-      id: 2
-    },
-    {
-      title: "Lord of Rings",
-      description: "...",
-      price: 200,
-      category: 2,
-      id: 3
-    }
-  ]
-
- 
-
-  const lul = arr.reduce((acc, item) => acc + item.price, 0)
-
-  console.log(lul)
-
-
-
-
-
-
-
-
-
-
-
-
   return (
     <>
       <div className="px-4">
